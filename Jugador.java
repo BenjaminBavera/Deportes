c51dd2faf85e03b61465a8955ec10324ec01057a
Benjamin Bavera <163863090+KulilinWP@users.noreply.github.com>
2024-06-27 01:15:06 -0300

    Update Jugador.java

--- Jugador.java
+++ Jugador.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 /**
  * Simula a un jugador y su salario.
  * 
- * @author Benjamin Bavera
+ * @author KulilinWP
  * @version 0.1
  */
 public class Jugador
@@ -10,27 +10,52 @@ public class Jugador
     private String nombre;
     private int sueldo;
    
-   public Jugador(String nombre, int sueldo)
+    /**
+     * Crea un objeto Jugador con el nombre indicado y sueldo indicado.
+     * precondicion: nombre no debe ser null o vacio y sueldo no debe ser menor que 0.
+     * @param nombre: es el nombre del jugador.
+     * @param sueldo: es el suelo del jugador.
+     */
+    public Jugador(String nombre, int sueldo)
    {
+     if (nombre == null) throw new IllegallArgumentException("nombre no debe ser null");
+     if (nombre.size() == 0) throw new IllegalArgumentException("nombre no debe ser vacio"):
+     if (sueldo < 0) throw new IllegalArgumentException("sueldo no debe ser menor que 0");   
      this.nombre = nombre;
      this.sueldo = sueldo;
     }  
-    
+
+     /**
+     * Retorna el sueldo del jugador.
+     */
     public int getSueldo()
     {
         return sueldo;
     }
     
+    /**
+    * Retorna el nombre del jugador. 
+    */
     public String getNombre()
     {
         return nombre;
     }
+
+    /**
+    * Retorna true si un jugador es igual a otro.
+    * Precondicion: otro no debe ser null
+    * @param otro: es el jugador con el cual queremos comparar.
+    */
     
     public boolean equals(Jugador otro)
     {
+        if (otro == null) throw new IllegalArgumentException("otro no debe ser null");
         if (nombre == otro.nombre){
             if (sueldo == otro.sueldo){            
              } return true;
+            else {
+               return false;
+            }
         } else {
             return false;
         } 
